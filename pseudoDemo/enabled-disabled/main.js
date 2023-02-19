b517e606067545d41e5a5e64838cf637b8a2664b
Phil Georgiou <philipposgeorgiou7@gmail.com>
2023-02-19 12:48:26 +0000

    comments added

--- pseudoDemo/enabled-disabled/main.js
+++ pseudoDemo/enabled-disabled/main.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded',
   document
     .querySelector('#billing-checkbox')
     .addEventListener('change', toggleBilling)
+    // we pass in false to set the event handler for the bubbling phase 
 }, false);
 
 function toggleBilling() {
